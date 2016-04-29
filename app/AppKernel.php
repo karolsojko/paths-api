51ccd14a8728d3839a268350fceea7f2cb33e1b8
karolsojko <karolsojko@gmail.com>
2016-04-29 16:27:38 +0200

    Fix Varnish host parameter setting

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -53,5 +53,10 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+
+        $envParameters = $this->getEnvParameters();
+        $loader->load(function($container) use($envParameters) {
+            $container->getParameterBag()->add($envParameters);
+        });
     }
 }
